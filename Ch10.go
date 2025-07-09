7e2cfde28afcc75149cbfe9b74d239f0a8305bf6
Jesse-Jackson Zigi <59916143+ZigiJesse-Jackson@users.noreply.github.com>
2025-07-09 22:57:26 +0000

    v2.0.0 update: support for generic type

--- Ch10.go
+++ Ch10.go
@@ -1,6 +1,12 @@
 package Add
 
-// Add performs addition on int parameters a and b [https://www.mathsisfun.com/numbers/addition.html]
-func Add(a int, b int) int {
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+// Add performs addition on two numbers a and b and returns the result which is also a number of the same type. [https://www.mathsisfun.com/numbers/addition.html]
+func Add[T Number](a T, b T) T {
 	return a + b
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module Ch10
 
 go 1.23.2
+
+require golang.org/x/exp v0.0.0-20250620022241-b7579e27df2b
